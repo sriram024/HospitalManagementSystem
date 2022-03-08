a1531b12cc871e6499c58a96879cc7b444510b6b
vaishnaviyshu <vaishnavia205@gmai.com>
2022-03-08 15:29:39 +0530

    new changes

---
+++ HospitalDeviceManagement/src/main/java/com/ust/model/Employee.java
@@ -0,0 +1,41 @@
+package com.ust.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity; 
+import javax.persistence.GeneratedValue; import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor; import lombok.Data; 
+import lombok.NoArgsConstructor; import lombok.NonNull; 
+import lombok.RequiredArgsConstructor; 
+ 
+
+
+@Data 
+//@NoArgsConstructor 
+@RequiredArgsConstructor 
+@AllArgsConstructor 	
+@Entity 
+@Table(name="employee")
+public class Employee {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="empId")
+	private Integer empId;
+	
+	@Column(name="empName",length=20)
+	private String empName;
+	
+	@Column(name="designation")
+	private String designation;
+	
+	@Column(name="emailid",length=50)
+	private String emailid;
+	
+	@Column(name="phonenumber")
+	private Integer phonenumber;
+	
+	@Column(name="password",length=50)
+	private String password;
+}
--- HospitalDeviceManagement/src/main/java/com/ust/service/StudentServiceImpl.java
+++ HospitalDeviceManagement/src/main/java/com/ust/service/StudentServiceImpl.java
@@ -1,5 +1,58 @@
 package com.ust.service;
 
-public class StudentServiceImpl {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ust.model.Employee;
+import com.ust.repository.DocterRepo;
+
+
+@Service
+public class StudentServiceImpl implements DoctorService{
+
+	@Autowired
+	DocterRepo edao;
+	
+	@Override
+	public boolean isPresent(int id) {
+	
+		return edao.existsById(id);
+	}
+
+
+	@Override
+	public void updateEmployee(Employee e) {
+		// TODO Auto-generated method stub
+		edao.save(e);
+	}
+	
+	@Override
+	public Optional<Employee> getEmployeeById(int id) {
+//		edao.findById(id);
+		return edao.findById(id);
+	}
+
+	//====================================================
+	
+	@Override
+	public Integer addEmployee(Employee e) {
+		edao.save(e);
+		return e.getEmpId();
+	}
+
+	@Override
+	public List<Employee> listAllrecords() {
+		// TODO Auto-generated method stub
+		return edao.findAll();
+	}
+	
+	@Override
+	public void deleteById(int id) {
+		edao.deleteById(id);
+		
+	}
+	
 }
